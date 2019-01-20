8ceaee46c3cdf331a54178523d455d7d474db64d
leo96tush <41864567+leo96tush@users.noreply.github.com>
2019-01-20 20:19:55 +0530

    Create Beautiful Triplets.py

---
+++ Beautiful Triplets.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the beautifulTriplets function below.
+def beautifulTriplets(d, arr):
+    count = 0
+    for i in range(len(arr)):
+        k = arr[i]
+        if k+d in arr:
+            if k+2*d in arr:
+                count = count+1
+    return count
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = beautifulTriplets(d, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
